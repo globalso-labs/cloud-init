ab8b91af5c46dc3fb4cc53ad88e3359003e1afe1
Nick Aguilar <nickaguilarh@gmail.com>
2025-04-04 13:31:10 -0500

    feat: add jenkins
    Signed-off-by: Nick Aguilar <nickaguilarh@gmail.com>

---
+++ common/jenkins.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#
+# cloud-init
+# jenkins.sh
+# This file is part of cloud-init.
+# Copyright (c) 2025.
+# Last modified at Fri, 4 Apr 2025 13:30:48 -0500 by nick.
+#
+# DISCLAIMER: This software is provided "as is" without warranty of any kind, either expressed or implied. The entire
+# risk as to the quality and performance of the software is with you. In no event will the author be liable for any
+# damages, including any general, special, incidental, or consequential damages arising out of the use or inability
+# to use the software (that includes, but not limited to, loss of data, data being rendered inaccurate, or losses
+# sustained by you or third parties, or a failure of the software to operate with any other programs), even if the
+# author has been advised of the possibility of such damages.
+# If a license file is provided with this software, all use of this software is governed by the terms and conditions
+# set forth in that license file. If no license file is provided, no rights are granted to use, modify, distribute,
+# or otherwise exploit this software.
+#
+
+set -e
+
+echo "Updating system packages..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Java (required for Jenkins)..."
+sudo apt install -y openjdk-17-jdk
+
+echo "Adding Jenkins repository key..."
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo "Adding Jenkins repository..."
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+echo "Updating package list..."
+sudo apt update
+
+echo "Installing Jenkins..."
+sudo apt install -y jenkins
+
+echo "Enabling and starting Jenkins service..."
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+echo "Checking Jenkins status..."
+sudo systemctl status jenkins --no-pager
+
+echo "Retrieving initial admin password..."
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
